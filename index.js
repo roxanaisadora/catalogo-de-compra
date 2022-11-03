a2b19cb48d949392d35cc624db1a77a77d17a15a
roxana isadora <roxanaisadora@gmail.com>
2022-11-02 22:35:58 -0500

    add api.json
    Se agrega el array de los datos desde el documento externo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.getElementById("pricing").style.visibility = 'hidden';
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    fetchData()
+})
+
+const fetchData =async () =>{
+    try{
+        const res = await fetch('api.json')
+        const data = await res.json()
+        console.log(data)
+
+    }catch(error){
+        console.log(error)
+    }
+}
